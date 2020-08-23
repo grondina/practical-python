a7c6c7cb74a8ed636539ee1e8b01ecfafc2ab8d0
Gustavo Rondina <gustavo.rondina@covestro.com>
2020-08-23 17:43:03 +0200

    Exercise 1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,18 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+while principal > 0:
+    month += 1
+    principal = principal * (1 + rate/12) - payment
+    total_paid += payment
+    print('{:3d} {:>12.2f} {:>12.2f}'.format(month, total_paid, principal))
+
+print('Total paid', round(total_paid, 2))
+
