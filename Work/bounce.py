8fee1d5e820c03fc1c17f6c1e814386d1b5f634b
Gustavo Rondina <gustavo.rondina@covestro.com>
2020-08-23 17:02:57 +0200

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+factor = 3/5
+bounce = 0
+
+while bounce < 10:
+    height *= factor
+    bounce += 1
+    print(bounce, round(height, 4))
+
